64d0f0db46235f5ee01abcf9da2d7c2244cc3c05
sakibhossain22 <sakib01181@gmail.com>
2023-11-10 09:54:54 +0600

    contact section added

---
+++ src/Components/Home/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+
+const Contact = () => {
+    return (
+        <div>
+            <div className="bg-black text-white my-10 h-48 flex items-center justify-center">
+                <h1 className="text-4xl font-bold">Call Us: +88 0192345678910</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Contact;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "./Banner/Banner";
 import ChefServIce from "./ChefService/ChefServIce";
+import Contact from "./Contact/Contact";
 import Menu from "./Menu/Menu";
 import Slider from "./Slider/Slider";
 
@@ -10,6 +11,7 @@ const Home = () => {
            <Slider></Slider>
            <ChefServIce></ChefServIce>
            <Menu></Menu>
+           <Contact></Contact>
         </div>
     );
 };
