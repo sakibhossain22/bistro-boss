fdebf31ce6fccff434c322a9227c0056c3cb2b02
sakibhossain22 <sakib01181@gmail.com>
2023-11-10 09:51:31 +0600

    chef service section added

--- src/Components/Home/Menu/Menu.jsx
+++ src/Components/Home/Menu/Menu.jsx
@@ -5,14 +5,14 @@ import SectionTitle from "../../Shared/SectionTiitle/SectionTitle";
 const Menu = () => {
     const [menus, setMenus] = useState([])
     console.log(menus);
-    useEffect(()=> {
+    useEffect(() => {
         fetch('menu.json')
-        .then(res => res.json())
-        .then(data => {
-            const filter = data.filter(item => item.category == 'salad')
-            setMenus(filter);
-        })
-    },[])
+            .then(res => res.json())
+            .then(data => {
+                const filter = data.filter(item => item.category == 'salad')
+                setMenus(filter);
+            })
+    }, [])
     return (
         <div className="my-10">
             <SectionTitle heading='---Check it out---' subHeading='FROM OUR MENU'></SectionTitle>
@@ -20,6 +20,9 @@ const Menu = () => {
                 {
                     menus?.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
                 }
+            </div>
+            <div className="flex items-center justify-center">
+                <button className="btn px-6 py-2 btn-primary text-center mx-auto my-8">View All Menu</button>
             </div>
         </div>
     );
--- src/Components/Shared/SectionTiitle/SectionTitle.jsx
+++ src/Components/Shared/SectionTiitle/SectionTitle.jsx
@@ -1,7 +1,7 @@
 
 const SectionTitle = ({ heading, subHeading }) => {
 
-    return <div className="text-white w-4/12 mx-auto text-center my-10">
+    return <div className="text-black w-4/12 mx-auto text-center my-10">
         <p className="text-yellow-500 my-5">----{heading}----</p>
         <h1 className="text-4xl border-y-4 py-4">{subHeading}</h1>
     </div>
